77e81c9b2b2d7b39264acb838571bbdfafd69a4c
Garrett <gcamoin5@gmail.com>
2023-01-05 15:51:47 -0500

    commit tree 99d28a74ae57b19f49f4b6f241b029300760edec

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    employee: 'Garrett',
+    streetAddress: '713 Morgan Lane'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee [key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee}
+    delete newObject [key]
+    return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee [key]
+    return employee
+}
